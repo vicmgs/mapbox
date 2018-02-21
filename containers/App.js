89e3179d7b097b640566ac270f58265653ff98cf
Victor Mu <vic.mgs@gmail.com>
2018-02-20 16:28:34 -0800

    Set up boilerplate

---
+++ containers/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './App.css';
+
+export default class App extends Component {
+    render () {
+        return <p className="test" >This is my new react app</p>
+    }
+}
